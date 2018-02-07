ae6f2cfa3f5ae755a2117426126efbe0e5340816
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2018-02-07 17:16:28 +0800

    added async loading of fonts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Webfont from 'webfontloader'
 import Helmet from 'react-helmet'
 import './minireset.css'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-	<div>
-		<Helmet
-			title="Landslides & Sinkholes"
-			meta={[
-				{ name: 'description', content: 'Sample' },
-				{ name: 'keywords', content: 'sample, something' }
-			]}
-			link={[
-				{
-					href:
-						'href=https://fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i',
-					rel: 'stylesheet'
-				}
-			]}
-		/>
+class TemplateWrapper extends React.Component {
+  componentDidMount() {
+    Webfont.load({
+      google: {
+        families: ['Josefin Sans']
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="Landslides & Sinkholes"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' }
+          ]}
+        />
 
-		<React.Fragment>{children()}</React.Fragment>
-	</div>
-)
+        <React.Fragment>{children()}</React.Fragment>
+      </div>
+    )
+  }
+}
 
 TemplateWrapper.propTypes = {
-	children: PropTypes.func
+  children: PropTypes.func
 }
 
 export default TemplateWrapper
